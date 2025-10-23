74c0641339fc9c4d3c5cd940cf98880cafd2da53
aminucf2002 <amin.komeili@ucalgary.ca>
2025-10-22 20:14:16 -0600

    moved some fiels

---
+++ 100-days-of-code/Day18_Turtle_Graphical_User_Interface/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+timmy_the_turttle=Turtle()
+timmy_the_turttle.shape("turtle")
+timmy_the_turttle.color("red")
+timmy_the_turttle.forward(100)
+timmy_the_turttle.right(90)
+
+import heroes
+print(heroes.gen())
+
+
+
+
+
+
+
+
+screen=Screen()
+screen.exitonclick()
